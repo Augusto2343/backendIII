f016cbf42a62532e2db70fe4413cda45011839e1
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-10-31 09:05:56 -0300

    Subiendo modificaciones

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -7,7 +7,9 @@ export const initMongoDB = async(db)=>{
                 await connect(config.TEST_MONGO_URL)
             }
             else{
-                await connect(config.MONGO_URL);
+                await connect(config.MONGO_URL, {
+                    serverSelectionTimeoutMS:300000,
+                });
             }
         } catch (error) {
             throw new Error(error);
