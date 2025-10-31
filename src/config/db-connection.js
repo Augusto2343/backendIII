179fe4d5edf2cbf7ad55ef64bd75b7e2772dbde4
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-10-31 15:00:20 -0300

    Actualizando conexion de db

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -4,10 +4,14 @@ import config from "./config.js";
 export const initMongoDB = async(db)=>{
         try {
             if(db == "testing"){
-                await connect(config.TEST_MONGO_URL)
+                await connect(config.TEST_MONGO_URL,{
+                    dbName: "test"
+                })
             }
             else{
-                console.log(config.MONGO_URL);
+                console.log(config.MONGO_URL,{
+                    dbName: "api"
+                });
                 
                 await connect(config.MONGO_URL, {
                     serverSelectionTimeoutMS:300000,
