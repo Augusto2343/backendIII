2fe20f6040d8542be44939db66021a7ea68d7d2b
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-11-03 23:16:59 -0300

    probando solución 1

--- src/services/userService.js
+++ src/services/userService.js
@@ -46,8 +46,8 @@ class UserService {
             for (let i = 1; i <= cantidad; i++) {
                 let data = generateUserFaker();
                 console.log(data);
-                
-                data.password = createHash(data.password)
+                let contrasenia = createHash(data.password);
+                data.password =contrasenia
                 arrayUsers.push(data)
             }
             return await this.repo.create(arrayUsers)
