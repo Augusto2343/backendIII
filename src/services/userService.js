f2fec96abd7b7be3bfe4b9b8b541fc7646808a24
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-11-03 23:26:55 -0300

    controlando el loggin

--- src/services/userService.js
+++ src/services/userService.js
@@ -45,11 +45,10 @@ class UserService {
             const arrayUsers =[]
             for (let i = 1; i <= cantidad; i++) {
                 let data = generateUserFaker();
-                console.log(data);
                 
-                // let contrasenia = createHash(data.password);
-                // data.password =contrasenia
-                // arrayUsers.push(data)
+                 let contrasenia = createHash(data.password);
+                 data.password =contrasenia
+                 arrayUsers.push(data)
             }
             return await this.repo.create(arrayUsers)
         }catch(e){
